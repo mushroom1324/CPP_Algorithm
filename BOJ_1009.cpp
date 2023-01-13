55df2c4484a8c14ab7243de94d7928a5ae2a29e3
seochanhyeok <mushroom1324@naver.com>
2023-01-13 22:18:45 +0900

    First CPP file :)

---
+++ BOJ_1009.cpp
@@ -0,0 +1,19 @@
+//
+// Created by 서찬혁 on 2023/01/13.
+//
+#include <iostream>
+using namespace std;
+int main() {
+    int T;
+    cin >> T;
+    for(int i = 0; i < T; ++i) {
+        int a, b, ans = 1;
+        cin >> a >> b;
+        for(int j = 0; j < b; ++j) {
+            ans = ans * a % 10;
+        }
+        if(ans == 0) ans = 10;
+        cout << ans << endl;
+    }
+    return 0;
+}
